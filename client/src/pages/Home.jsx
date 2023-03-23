ba16ee83d3bb2ae39dc40515a7d63c4fbc5bb049
gia <giadatdoan@gmail.com>
2023-03-23 01:46:06 -0400

    .

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,7 +15,37 @@ const RenderCards = ({ data, title }) => {
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
-    const [searchText, setSearchText] = useState('')
+    const [searchText, setSearchText] = useState('');
+
+    useEffect(() => {
+      const fetchPosts = async () => {
+        setLoading(true);
+
+        try {
+            const response = await fetch('http://localhost:8080/api/v1/post', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+
+            if(response.ok) {
+                console.log('its ok')
+                const result = await response.json();
+                console.log('res', result)
+
+                setAllPosts(result.data.reverse());
+            }
+        } catch (error) {
+            alert(error);
+        } finally {
+            setLoading(false);
+        }
+      }
+
+      fetchPosts();
+    }, [])
+    
 
   return (
     <section className='max-w-7xl mx-auto'>
@@ -48,7 +78,7 @@ const Home = () => {
                             />
                         ) : (
                             <RenderCards
-                                data={[]}
+                                data={allPosts}
                                 title="No posts found"
                             />
                         )}
